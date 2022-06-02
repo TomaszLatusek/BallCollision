22b26ab323e91cfa51625336be2b1f96aa93986e
Tomasz Latusek <tomaszlatusek@wp.pl>
2022-06-02 14:21:28 +0200

    Add threads.

--- src/main/java/com/example/ballsdemo/Ball.java
+++ src/main/java/com/example/ballsdemo/Ball.java
@@ -1,85 +1,117 @@
 package com.example.ballsdemo;
 
 import javafx.geometry.Bounds;
-import javafx.scene.Group;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 import java.util.ArrayList;
 
 public class Ball {
 
-    public double x, y, r, m, vx, vy, tempVx, vxf;
+    public double x, y, r, m, vx, vy, tempVx, tempVy, vxf;
     public Circle display;
+    Object lock;
 
-    public Ball(double x, double y, double r, double m) {
+    public Ball(double x, double y, double r, double m, Object lock) {
         this.x = x;
         this.y = y;
         this.r = r;
         this.m = m;
+        this.lock = lock;
 
-        vx = (int) (-5 + Math.random() * 10);
-        vy = (int) (-5 + Math.random() * 10);
+        vx = (int) (3 + Math.random() * 3);
+        vy = (int) (3 + Math.random() * 3);
 
         display = new Circle();
         display.setCenterX(x);
         display.setCenterY(y);
         display.setRadius(r);
+
+        display.setFill(Color.color(Math.random(), Math.random(), Math.random()));
     }
 
     public void move() {
         x += vx;
         y += vy;
-        display.setLayoutX(display.getLayoutX() + vx);
-        display.setLayoutY(display.getLayoutY() + vy);
+    }
+
+    public void oneDim(Ball otherBall) {
+        tempVx = vx;
+        vx = ((m - otherBall.m) * vx + 2 * otherBall.m * otherBall.vx) / (m + otherBall.m);
+        otherBall.vx = ((otherBall.m - m) * otherBall.vx + 2 * m * tempVx) / (m + otherBall.m);
     }
 
     public void detectCollision(ArrayList<Ball> balls) {
         for (Ball otherBall : balls) {
             if (otherBall != this) {
                 if (collisionDetected(otherBall)) {
-                    handleCollision(otherBall, false);
+                    handleCollision(otherBall);
+//                    oneDim(otherBall);
                 }
             }
         }
     }
 
     private boolean collisionDetected(Ball otherBall) {
-        return display.getBoundsInParent().intersects(otherBall.getBounds());
-       // return Math.sqrt(Math.pow(Math.abs(x - otherBall.x), 2) + Math.pow(Math.abs(y - otherBall.y), 2)) <= r + otherBall.r;
+        // return getBounds().intersects(otherBall.getBounds());
+        return (Math.sqrt(Math.pow(Math.abs(x - otherBall.x), 2) + Math.pow(Math.abs(y - otherBall.y), 2)) <= (r + otherBall.r));
     }
 
     public void checkCollisionWithBorder(Bounds border) {
-        if (getBounds().getMaxX() >= border.getMaxX() || getBounds().getMinX() <= border.getMinX()) {
+        if (x + r >= border.getMaxX() || x - r <= border.getMinX()) {
             vx *= -1;
         }
-        if (getBounds().getMaxY() >= border.getMaxY() || getBounds().getMinY() <= border.getMinY()) {
+        if (y + r >= border.getMaxY() || y - r <= border.getMinY()) {
             vy *= -1;
         }
     }
 
-    public void handleCollision(Ball otherBall, boolean secondCall) {
-        if (!secondCall) {
-            double angle = Math.atan2(Math.abs(getX() - otherBall.getX()), Math.abs(getY() - otherBall.getY()));
-            tempVx = Math.cos(angle) * vx + Math.sin(angle) * vy;
-            vxf = ((m - otherBall.m) * tempVx + 2 * otherBall.m * otherBall.tempVx) / (m + otherBall.m);
+    public void handleCollision(Ball otherBall) {
 
-            vx = (Math.cos(angle) * vxf - Math.sin(angle) * vy);
-            vy = (Math.sin(angle) * vx + Math.cos(angle) * vy);
+        // synchronized (lock) {
+        double angle = Math.atan2(Math.abs(getX() - otherBall.getX()), Math.abs(getY() - otherBall.getY()));
 
-            otherBall.handleCollision(this, true);
-        }
+        tempVx = Math.cos(angle) * vx + Math.sin(angle) * vy;
+        tempVy = -Math.sin(angle) * vx + Math.cos(angle) * vy;
+        otherBall.tempVx = Math.cos(angle) * otherBall.vx + Math.sin(angle) * otherBall.vy;
+        otherBall.tempVy = -Math.sin(angle) * otherBall.vx + Math.cos(angle) * otherBall.vy;
 
+        vxf = ((m - otherBall.m) * tempVx + 2 * otherBall.m * otherBall.tempVx) / (m + otherBall.m);
+        otherBall.vxf = ((otherBall.m - m) * otherBall.tempVx + 2 * m * tempVx) / (m + otherBall.m);
+
+        vx = (Math.cos(angle) * vxf - Math.sin(angle) * tempVy);
+        vy = (Math.sin(angle) * vxf + Math.cos(angle) * tempVy);
+
+        otherBall.vx = (Math.cos(angle) * otherBall.vxf - Math.sin(angle) * otherBall.tempVy);
+        otherBall.vy = (Math.sin(angle) * otherBall.vxf + Math.cos(angle) * otherBall.tempVy);
+        //}
     }
 
-    public Bounds getBounds() {
+    public synchronized Bounds getBounds() {
         return display.getBoundsInParent();
     }
 
-    public double getX() {
+    public void compute(ArrayList<Ball> balls, Bounds scene) throws InterruptedException {
+        while (true) {
+            move();
+            detectCollision(balls);
+            checkCollisionWithBorder(scene);
+            animate();
+            Thread.sleep(10);
+        }
+    }
+
+    public void animate() {
+        display.setLayoutX(x);
+        display.setLayoutY(y);
+    }
+
+    public synchronized double getX() {
         return x;
     }
 
-    public double getY() {
+    public synchronized double getY() {
         return y;
     }
+
 }
--- src/main/java/com/example/ballsdemo/Main.java
+++ src/main/java/com/example/ballsdemo/Main.java
@@ -1,64 +1,79 @@
 package com.example.ballsdemo;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.geometry.Bounds;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class Main extends Application {
     private Pane canvas;
+    private Object lock;
     ArrayList<Ball> balls = new ArrayList<>();
-    private double vx1 = 3, vx2 = 0;
-    private double vy1 = 2, vy2 = 3;
 
     @Override
     public void start(final Stage primaryStage) throws FileNotFoundException {
 
         canvas = new Pane();
+        lock = new Object();
         final Scene scene = new Scene(canvas, 800, 600);
-        scene.setFill(Color.LIGHTGREEN);
+        scene.setFill(Color.BLACK);
 
         primaryStage.setTitle("Balls");
         primaryStage.setScene(scene);
 
-        Ball ball1 = new Ball(100, 100, 50, 15);
-        Ball ball2 = new Ball(300,100,50,10);
-        Ball ball3 = new Ball(300,200,50,10);
-        Ball ball4 = new Ball(200,300,50,10);
+        Ball ball1 = new Ball(0, 0, 60, 20, lock);
+        Ball ball2 = new Ball(0, 0, 60, 20, lock);
+//        Ball ball3 = new Ball(300, 200, 30, 10, lock);
+//        Ball ball4 = new Ball(200, 300, 20, 5, lock);
         balls.add(ball1);
         balls.add(ball2);
 //        balls.add(ball3);
 //        balls.add(ball4);
+
+        Thread th1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ball1.compute(balls, canvas.getBoundsInParent());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread th2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ball2.compute(balls, canvas.getBoundsInLocal());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        AnimationTimer updater = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                for (Ball ball : balls) {
+                    ball.animate();
+                }
+            }
+        };
 
         //add to stage
         for (Ball ball : balls) {
             canvas.getChildren().add(ball.display);
         }
 
-        final Timeline loop = new Timeline(new KeyFrame(Duration.millis(10), t -> {
-
-            for (Ball ball : balls) {
-                ball.move();
-                ball.detectCollision(balls);
-                ball.checkCollisionWithBorder(canvas.getBoundsInLocal());
-            }
-        }));
-
         primaryStage.show();
-        loop.setCycleCount(Timeline.INDEFINITE);
-        loop.play();
+        //updater.start();
+        th1.start();
+        th2.start();
     }
 
     public static void main(String[] args) {
